aeaf3c5ae1ae2259d5ce58b37c5594c8d8645be3
Pranav Ashok <pranavashok@gmail.com>
2013-04-01 22:28:52 +0530

    Dictionaries for alpha and beta, converted sample to list

--- HMM/calculator.py
+++ HMM/calculator.py
@@ -5,58 +5,36 @@ class HMM(object):
 		self.Pi=pi
 		self.Tran=tran
 		self.Emm=emm
-		self.Alpha=[]
-		self.Beta=[]
-	def generate( self ):
-		for key,value in self.Pi.iteritems():
-			state=State(key,value)
-			self.States.append(state)
-		for state in self.States:
-			state.tran=self.Tran[state.data]
-			state.emm=self.Emm[state.data]
+		self.Alpha={}
+		self.Beta={}
+	def generate(self):
+		for key, value in self.Pi.iteritems():
+			self.States.append(key)
 	def forward( self, learnSample):
 		for r in learnSample:
-			Alpha=[]
-			Alpha2=[]
-			temp=0
-			for j in range(0 , len(r) ):
-				if ( j == 0 ):
+			for j in xrange(0, len(r)):
+				if j == 0:
 					for state in self.States:
-						alpha =  self.Pi[state.data]*self.Emm[state.data][r[j]]
-						Alpha.append({state.data : alpha })
-					self.Alpha.append( {j+1 : Alpha} )
+						self.Alpha[(" ".join(r), j+1, state)] = self.Pi[state]*self.Emm[(state, r[j])]
 				else:
-					for state in self.States:
-						alpha=0
-						for s in self.States:		#requires editting
-							for value in Alpha:
-								alpha +=value.get(s.data,0)*self.Tran[s.data][state.data]*self.Emm[state.data][r[j]]
-						Alpha2.append({state.data : alpha })
-					Alpha=Alpha2
-					self.Alpha.append( {j+1 : Alpha} )
-					Alpha2=[]	
-		print self.Alpha
+					for s1 in self.States:
+						alpha = 0
+						for s2 in self.States:
+							alpha += self.Alpha[(" ".join(r), j, s2)]*self.Tran[(s2, s1)]*self.Emm[(s1, r[j])]
+						self.Alpha[(" ".join(r), j+1, s1)] = alpha
+		#print self.Alpha
 	
 	def backward( self,learnSample):
 		for r in learnSample:
-			r=r[::-1]
-			Beta=[]
-			Beta2=[]
-			temp=0
-			for state in self.States:
-				Beta.append( {state.data : 1 } )
-			self.Beta.append( { len(r) : Beta } )
-			for j in range( 0,len(r)-1 ):
-				for state in self.States:
-					beta=0
-					for s in self.States:		#requires editting
-						for value in Beta:
-							beta +=value.get(s.data,0)*self.Tran[state.data][s.data]*self.Emm[s.data][r[j]]
-							#print value.get(s.data,0),self.Tran[state.data][s.data],self.Emm[s.data][r[j]],beta, r[j]
-					Beta2.append({state.data : beta })
-				Beta=Beta2
-				self.Beta.append( { len(r)-j-1 : Beta} )
-				Beta2=[]
-			print self.Beta
+			rdash = r[:]
+			rdash.reverse()
 
-	def gamma( self, learnSample ):
+			for state in self.States:
+				self.Beta[(" ".join(r), len(r), state)] = 1
+
+			for j in xrange(0, len(r)-1):
+				for s1 in self.States:
+					beta = 0
+					for s2 in self.States:
+							beta += self.Beta[(" ".join(r), len(r)-j, s2)]*self.Tran[(s1, s2)]*self.Emm[(s2, rdash[j])]
+					self.Beta[(" ".join(r), len(r)-j-1, s1)] = beta
--- HMM/sampledata.py
+++ HMM/sampledata.py
@@ -1,12 +1,14 @@
 from calculator import HMM
 v = {"s": {  "t" :.7 ,  "r" : .2  }, "t" :{ "s" : 6 } }
 Alpha=[]
-learnSample={"abba","bab" }
-model= HMM( { "s": .85, "t" : .15}, 
-		{"s" : {  "t" :.7 ,  "s" : .3  },"t" : { "s" : .1 , "t" : .9} }, 
-		{"s" : { "a" : .4, "b": .6}, "t" : { "a" : .5, "b": .5} } )
+learnSample = [['a', 'b', 'b', 'a'], ['b', 'a', 'b']]
+
+model= HMM({ "s": .85, "t" : .15}, 
+		{("s", "t") :.7 ,  ("s", "s") : .3 , ("t", "s"): .1 , ("t", "t") : .9}, 
+		{("s", "a"): .4, ("s", "b"): .6, ("t", "a"): .5, ("t", "b"): .5})
 model.generate()
 model.forward(learnSample)
+model.backward(learnSample)
 #for key,value in Alpha:
 #	print value
 	
